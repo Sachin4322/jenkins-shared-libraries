291d9bdf91b20550cdd06693363082315c05c77d
Sachin4322 <sachinshinde390@gmail.com>
2026-07-02 13:11:17 +0400

    Add Docker_push pipeline function for image handling

---
+++ vars/Docker_push.groovy
@@ -0,0 +1,14 @@
+def call(String credId,string imagename){ 
+  withCredentials([usernamePassword(
+                    credentialsId: '${credId}',
+                    usernameVariable: 'dockerHubUser',
+                    passwordVariable: 'dockerHubPass'
+                )]) {
+
+                    sh '''
+                    echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin
+                    docker tag '${imagename}' $dockerHubUser/'${imagename}'
+                    docker push $dockerHubUser/'${imagename}':latest
+                    '''
+                }
+}
